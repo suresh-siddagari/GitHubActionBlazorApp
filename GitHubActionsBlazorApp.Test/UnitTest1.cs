10c0ede03b54c4d4fa58ad0d5a99db9a2cad08d0
Suresh Siddagari <Suresh.Siddagari@csgi.com>
2024-12-16 12:09:04 +0100

    restructure

---
+++ GitHubActionsBlazorApp.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace GitHubActionsBlazorApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //testing
+            Assert.True(true);
+        }
+    }
+}
